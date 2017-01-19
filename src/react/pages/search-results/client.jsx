7a4306c8b42230f8d2490be8342fcd3410f29dcb
Michael Rawlings <ml.rawlings@gmail.com>
2017-01-19 15:13:16 -0800

    use preminified build - it doesn't actually affect the benchmarks, but just to be clear we're doing the right thing

--- src/react/pages/search-results/client.jsx
+++ src/react/pages/search-results/client.jsx
@@ -1,8 +1,8 @@
 // React looks at process.env.NODE_ENV to enable/disable performance gathering...
 process.env.NODE_ENV = 'production';
 
-var React = require('react');
-var ReactDOM = require('react-dom');
+var React = require('react/dist/react.min.js');
+var ReactDOM = require('react-dom/dist/react-dom.min.js');
 
 var SearchResults = require('~/src/react/components/SearchResults');
 
